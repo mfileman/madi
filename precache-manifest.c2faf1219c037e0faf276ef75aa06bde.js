20e82b68e7ce78ccf7aefda20755edb932e226d5
mfileman <madisonfileman@gmail.com>
2019-11-10 12:36:30 -0500

    deleted build folder

---
+++ precache-manifest.c2faf1219c037e0faf276ef75aa06bde.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7502d6ff09d5b5257382abac0cfd86f6",
+    "url": "/madi/static/media/download_icon.7502d6ff.png"
+  },
+  {
+    "revision": "dd4a3af4ed0e93cc4a3b610cbcf5c9ba",
+    "url": "/madi/static/media/Pixel12x10.dd4a3af4.ttf"
+  },
+  {
+    "revision": "9958b460c487f5be31cf",
+    "url": "/madi/static/js/runtime~main.7591000e.js"
+  },
+  {
+    "revision": "ad0b154f3602f77a2d53",
+    "url": "/madi/static/js/main.e09a0051.chunk.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/madi/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "ad0b154f3602f77a2d53",
+    "url": "/madi/static/css/main.54e4329e.chunk.css"
+  },
+  {
+    "revision": "86e4b0bc917701216e06e1f3efd79318",
+    "url": "/madi/index.html"
+  }
+];
